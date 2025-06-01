50c84b91f9b4d0efc210ad7f1a6a55f48c6ef79b
IOBA001 <nklv717@gmail.com>
2025-06-01 21:31:58 +0400

    finish

---
+++ script.js
@@ -0,0 +1,14 @@
+
+
+let userName = "ana";
+let age = "25";
+let status = false;
+let x = null;
+let y;
+
+
+
+let number = '10';
+let number2= '5'
+let result = number1 = number2
+console.log(result)
